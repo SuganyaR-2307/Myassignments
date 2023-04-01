0b8e34b47be3b5d31a7bdd499ecf66badbb26da9
SuganyaR-2307 <129620023+SuganyaR-2307@users.noreply.github.com>
2023-04-01 18:22:45 +0000

    Add files via upload
    day1

---
+++ day1/Learnvariables.java
@@ -0,0 +1,21 @@
+package weekone.day1;
+
+public class Learnvariables {
+	public static void main(String[] args) {
+		int num1=100;
+		float num2=12.345f;
+		char letter='A';
+		double num3=945229839.341341;
+		long num4=98763553456378L;
+		String name ="Suganya";
+		boolean result=true;
+		System.out.println("Integer value is "+num1);
+		System.out.println("float value is "+num2);
+		System.out.println("Character value is "+letter);
+		System.out.println("Double value is "+num3);
+		System.out.println("long value is "+num4);
+		System.out.println("String value is "+name);
+		System.out.println("Boolean value is "+result);
+	}
+
+}
